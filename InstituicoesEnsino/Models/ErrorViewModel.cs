9e9c11a6a2ff078e1e029ad8c222403d75dc6053
fabiocostanegro <fabiocostanegro@gmail.com>
2022-06-01 17:44:05 -0300

    Projeto base para aprender ASPNET MVC CORE

---
+++ InstituicoesEnsino/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InstituicoesEnsino.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
