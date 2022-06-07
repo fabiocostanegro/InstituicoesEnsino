1ea2d883c530bc6551952866b25d2992c26d9e45
fabiocostanegro <fabiocostanegro@gmail.com>
2022-06-07 07:48:01 -0300

    Criação da view para listagem de instituições (continuação)

---
+++ InstituicoesEnsino/Controllers/InstituicoesController.cs
@@ -0,0 +1,22 @@
+﻿using InstituicoesEnsino.Data;
+using InstituicoesEnsino.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InstituicoesEnsino.Controllers
+{
+    public class InstituicoesController : Controller
+    {
+        private Context dbContext;
+        public InstituicoesController(Context context)
+        {
+            dbContext = context;    
+        }
+        public IActionResult Index()
+        {
+            List<Instituicao> instituicao = dbContext.Instituicoes.ToList();
+            return View(instituicao);
+        }
+    }
+}
