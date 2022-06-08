d6d8bd6d4e866b205119b5becf9234bb54df650b
fabiocostanegro <fabiocostanegro@gmail.com>
2022-06-08 07:38:09 -0300

    Implementação do metodo de edição

--- InstituicoesEnsino/Controllers/InstituicoesController.cs
+++ InstituicoesEnsino/Controllers/InstituicoesController.cs
@@ -30,5 +30,18 @@ namespace InstituicoesEnsino.Controllers
             dbContext.SaveChanges();
             return RedirectToAction("Index");
         }
+        public IActionResult Edit(long id)
+        {
+            Instituicao instituicao = dbContext.Instituicoes.Where(i => i.InstituicaoID == id).First();
+            return View(instituicao);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Instituicao instituicao)
+        {
+            dbContext.Instituicoes.Update(instituicao);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
