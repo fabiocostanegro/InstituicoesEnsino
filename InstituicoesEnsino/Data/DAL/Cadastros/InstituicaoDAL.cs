875d613d6c1bb895f34d12be6c93699f9757e09c
fabiocostanegro <fabiocostanegro@gmail.com>
2022-06-20 19:23:07 -0300

    Criação da classe de persistencia para instituições

---
+++ InstituicoesEnsino/Data/DAL/Cadastros/InstituicaoDAL.cs
@@ -0,0 +1,22 @@
+﻿using Modelo.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituicoesEnsino.Data.DAL.Cadastros
+{
+    public class InstituicaoDAL
+    {
+        private Context dbContext;
+        public InstituicaoDAL(Context context)
+        {
+            dbContext = context;
+        }
+        public IQueryable<Instituicao> ObterInstituicoesClassificadasPorNome()
+        {
+            return dbContext.Instituicoes.OrderBy(i => i.Nome);
+        }
+
+    }
+}
