cf4637240ee7148a3d587794d29d3f6bd171d033
fabiocostanegro <fabiocostanegro@gmail.com>
2022-06-27 07:29:31 -0300

    Criação das classes professor e curso professor

---
+++ Modelo/Docente/CursoProfessor.cs
@@ -0,0 +1,10 @@
+﻿namespace Modelo.Docente
+{
+    public class CursoProfessor
+    {
+        public long? CursoID { get; set; }
+        public Curso Curso { get; set; }
+        public long? ProfessorID { get; set; }
+        public Professor Professor { get; set; }
+    }
+}
